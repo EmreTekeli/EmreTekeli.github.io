84f1f343289fda749ac93e85789efa141384139c
EmreTekeli <tekeliemre@outlook.com.tr>
2023-03-22 13:34:22 +0300

    Create lang-en.js

---
+++ frontend/js/lang-en.js
@@ -0,0 +1,29 @@
+lang = (function () {
+    "use strict";
+    return {
+        product: "product",
+        slg_product: "product",
+        products: "products",
+        slg_products: "products",
+
+        brand: "brand",
+        slg_brand: "brand",
+        brands: "brands",
+        slg_brands: "brands",
+
+        index: "index",
+        slg_index: "index",
+        indexes: "indexes",
+        slg_indexes: "indexes",
+
+        type: "type",
+        slg_type: "type",
+        types: "types",
+        slg_types: "types",
+
+        newspaper: "newspaper",
+        slg_newspaper: "newspaper",
+        newspapers: "newspapers",
+        slg_newspapers: "newspapers",
+    };
+})();
