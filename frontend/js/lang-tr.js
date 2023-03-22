071bd9d7eecb0da92ab4f3af1bdfe61efe122fbf
EmreTekeli <tekeliemre@outlook.com.tr>
2023-03-22 13:34:24 +0300

    Create lang-tr.js

---
+++ frontend/js/lang-tr.js
@@ -0,0 +1,30 @@
+lang = (function () {
+    "use strict";
+    return {
+        product: "ürün",
+        slg_product: "urun",
+        products: "ürünler",
+        slg_products: "urunler",
+
+        brand: "firma",
+        slg_brand: "firma",
+        brands: "firmalar",
+        slg_brands: "firmalar",
+
+        index: "indeks",
+        slg_index: "indeks",
+        indexes: "indeksler",
+        slg_indexes: "indeksler",
+
+        type: "type",
+        slg_type: "type",
+        types: "types",
+        slg_types: "types",
+
+        newspaper: "newspaper",
+        slg_newspaper: "newspaper",
+        newspapers: "newspapers",
+        slg_newspapers: "newspapers",
+    };
+})();
+
